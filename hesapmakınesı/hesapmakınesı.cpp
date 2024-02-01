8a1b75a4d117802e2351c2d6637f927e99b8ebd2
1myness <152972743+1myness@users.noreply.github.com>
2024-02-01 13:13:40 +0300

    Proje dosyası ekle.

---
+++ hesapmakınesı/hesapmakınesı.cpp
@@ -0,0 +1,35 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int sayi1, sayi2;
+	cout << "iki deger giriniz" << endl;
+	cin >> sayi1 >> sayi2;
+
+	int toplam;
+	toplam = sayi1 + sayi2;
+
+	int fark;
+	fark = sayi1 - sayi2;
+	
+	int carpim;
+	carpim = sayi1 * sayi2;
+
+	int bolum;
+	bolum = sayi1 / sayi2;
+
+	cout << sayi1 << "+" << sayi2 << "=" << toplam << endl;
+	cout << sayi1 << "-" << sayi2 << "=" << fark << endl;
+	cout << sayi1 << "*" << sayi2 << "=" << carpim << endl;
+	cout << sayi1 << "/" << sayi2 << "=" << bolum << endl;
+
+
+
+
+	return 0;
+}
+
